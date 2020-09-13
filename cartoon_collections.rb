234800ca35e3ec834aeef40b00e5445504f830a6
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-13 22:10:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.detect do |i| 
-    if i == cheese_types
+    if i = cheese_types
       puts i
     else 
       puts "no"
