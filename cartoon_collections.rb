58cb89c6a460b2d57c8a752ca01126c89b67d7eb
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-13 22:09:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect do |i| i.is_a?(cheese_types)
+  end
 end
